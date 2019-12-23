a1e536c1d5f4def423dbadfda3d61074fe2bbe69
Shivam <patelsd96@gmail.com>
2019-12-23 18:16:47 +0000

    packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,14 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 console.log("Kitchen stuff to pack:");
 console.log(packingList.length)
-for(let i = 0; i < packingList.length; i++){
-  console.log(packingList[i]);
-  console.log(i);
+// for(let i = 0; i < packingList.length; i++){
+//   console.log(packingList[i]);
+//   console.log(i);
 
+// }
+
+let i = 0;
+while(i <packingList.length){
+  console.log(packingList[i]);
+  i++;
 }
